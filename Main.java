5601fb0ddac8e11b3a834bb839e4b836a10acd62
lizziedelmonico <123976957+lizziedelmonico@users.noreply.github.com>
2023-12-08 14:11:42 -0500

    WOOO THE GRAPH WORKS PROF RANDO IS A GENIUS

--- Main.java
+++ Main.java
@@ -3,48 +3,59 @@
 import com.google.common.graph.*;
 import java.awt.Color;
 import java.util.Scanner;
+import java.util.HashMap;
 
 class Main{
 
   public static void main(String[] args){
     // Initialize graph
-    MutableNetwork<String,String> network = NetworkBuilder.directed().build();
+    MutableValueGraph<String,String> network = ValueGraphBuilder.directed().build();
+    HashMap<String, String> tributes = new HashMap<>();
+
 
     // Specify where data is stored
     Scanner file = ReadFile.read("data_file.txt");
 
-    // Read file line by line
-    Integer lineCount = 0;
-    while (file.hasNextLine() && lineCount < 10) {
+    // Read file line by lin
+    while (file.hasNextLine()) {
       String line = file.nextLine();
-      System.out.println(line);
       String[] fields = line.split("\\s+");
-      if(fields[0].equals("e")){
-        if(fields[3].equals("Targeted") && fields[4].equals(""))
-          network.addEdge(fields[1], fields[2], "Targeted");
+      System.out.println(fields[0]);
+      if(fields[0].equals("n")){
+        network.addNode(fields[2]);
+        tributes.put(fields[1], fields[2]);
       }
-        if(fields[3].equals("Revenge")){
-          network.addEdge(fields[1], fields[2], "Revenge Targeted");
-        }
-        if(fields[3].equals("Targeted") && fields[4].equals("Indirect")){
-          network.addEdge(fields[1], fields[2], "Targeted Indirect");
-        }
-        if(fields[3].equals("Accidental")){
-          network.addEdge(fields[1], fields[2], "Accidental Indirect");
-        }
-        if(fields[3].equals("Mercy")){
-          network.addEdge(fields[1], fields[2], "Mercy");
-        }
-      }
-      lineCount += 1;
-    file.close();
-    System.out.println(network);
-    GraphDisplay d3 = new GraphDisplay(network);
-    d3.setNodeColors(Color.GREEN);
-    d3.setEdgeColors(Color.BLUE);
-    for (Object n : d3.getNodeSet()) {
-      System.out.println(n+" : "+d3.getLabel(n));
-    }
+       if(fields[0].equals("e")){
+        String person1 = fields[1];
+        String person2 = fields[2];
+        System.out.println(fields.length);
+          if(fields[3].equals("Targeted")){
+            if(fields.length == 4){
+              network.putEdgeValue(tributes.get(person1), tributes.get(person2), "Targeted");
+            }
+            else if(fields.length > 4){
+              network.putEdgeValue(tributes.get(person1), tributes.get(person2), "Targeted Indirect");
+            }
+
+          }
+           else if(fields[3].equals("Revenge")){
+             network.putEdgeValue(tributes.get(person1), tributes.get(person2), "Revenge Targeted");
+           }
+           else if(fields[3].equals("Accidental")){
+             network.putEdgeValue(tributes.get(person1), tributes.get(person2), "Accidental Indirect");
+           }
+           else if(fields[3].equals("Mercy")){
+             network.putEdgeValue(tributes.get(person1), tributes.get(person2), "Mercy");
+           }
+      }}
+      file.close();
+     System.out.println(network);
+     GraphDisplay d3 = new GraphDisplay(network);
+     d3.setNodeColors(Color.PINK);
+     d3.setEdgeColors(Color.BLACK);
+     for (Object n : d3.getNodeSet()) {
+       System.out.println(n+" : "+d3.getLabel(n));
+     }
     for (Object e : d3.getEdgeSet()) {
       System.out.println(e+" : "+d3.getLabel(e));
     }
